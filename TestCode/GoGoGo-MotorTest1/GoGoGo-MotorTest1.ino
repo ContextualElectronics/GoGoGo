69a9c4e902d46cfc779d95d9dbd06c9724e6cc7a
Chris Gammell <chris@contextualelectronics.com>
2015-12-05 23:12:53 -0500

    Updated motor test to work with the new GoGoGo Motor Driver Rev 2 board

--- TestCode/GoGoGo-MotorTest1/GoGoGo-MotorTest1.ino
+++ TestCode/GoGoGo-MotorTest1/GoGoGo-MotorTest1.ino
@@ -1,34 +1,52 @@
 // the setup function runs once when you press reset or power the board
+
+int in1 = 1;
+int in2 = 4;
+int stby = 0;
+int pwm = 5;
+int decoder = 2;
+
+int counter=0;
+
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
-  
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(0, OUTPUT);
+  pinMode(2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(decoder), count, CHANGE);
+  Serial.begin(9600);
 }
+
+
+
+void count() {
+  counter++; 
+}
+
+
+
 
 // the loop function runs over and over again forever
 void loop() {
-  int in1 = 13;
-  int in2 = 12;
-  int stby = 11;
-  int pwm = 10;
-  
-  
+
   digitalWrite(pwm, HIGH);   
   digitalWrite(stby, HIGH);   
   digitalWrite(in1, HIGH);   
-  digitalWrite(in2, LOW);   
+  digitalWrite(in2, LOW); 
+  delay(200);   
+  Serial.println("Forward");  
+  Serial.println(counter);
   delay(2000);              // wait for a second
   digitalWrite(pwm, HIGH);   
   digitalWrite(stby, HIGH);   
   digitalWrite(in1, HIGH);   
-  digitalWrite(in2, HIGH);   
+  digitalWrite(in2, HIGH);
+  delay(200); 
+  Serial.println("Stop");    
+  Serial.println(counter);
   delay(2000);              // wait for a second
-  digitalWrite(pwm, HIGH);   
-  digitalWrite(stby, HIGH);   
-  digitalWrite(in1, LOW);   
-  digitalWrite(in2, HIGH); 
-  delay(2000);              // wait for a second
+
+  
 }
