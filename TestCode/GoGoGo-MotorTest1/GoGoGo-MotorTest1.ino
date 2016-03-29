20bf7e11e495a06130a1be8fb42c245cf4e9f1c9
Chris Gammell <chris@contextualelectronics.com>
2016-03-29 00:53:31 -0400

    Updated code to control wheel by number of steps

--- TestCode/GoGoGo-MotorTest1/GoGoGo-MotorTest1.ino
+++ TestCode/GoGoGo-MotorTest1/GoGoGo-MotorTest1.ino
@@ -1,20 +1,22 @@
 // the setup function runs once when you press reset or power the board
 
-int in1 = 1;
-int in2 = 4;
+int in1 = 6;
+int in2 = 8;
 int stby = 0;
 int pwm = 5;
 int decoder = 2;
+int countLimit = 10000;
+
 
 int counter=0;
 
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(0, OUTPUT);
-  pinMode(2, INPUT);
+  pinMode(pwm, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(stby, OUTPUT);
+  pinMode(decoder, INPUT);
   attachInterrupt(digitalPinToInterrupt(decoder), count, CHANGE);
   Serial.begin(9600);
 }
@@ -22,7 +24,7 @@ void setup() {
 
 
 void count() {
-  counter++; 
+  ++counter; 
 }
 
 
@@ -31,22 +33,34 @@ void count() {
 // the loop function runs over and over again forever
 void loop() {
 
-  digitalWrite(pwm, HIGH);   
+  analogWrite(pwm, 128);   
+  counter = 0;
+  while(counter<30){
   digitalWrite(stby, HIGH);   
   digitalWrite(in1, HIGH);   
   digitalWrite(in2, LOW); 
-  delay(200);   
-  Serial.println("Forward");  
   Serial.println(counter);
-  delay(2000);              // wait for a second
-  digitalWrite(pwm, HIGH);   
-  digitalWrite(stby, HIGH);   
+  }
+  Serial.print("Forward out of the loop now");
   digitalWrite(in1, HIGH);   
+  digitalWrite(in2, HIGH);             
+  delay(2000);   
+  
+  analogWrite(pwm, 255);   
+  counter = 0;
+  while(counter<30){
+  digitalWrite(stby, HIGH);   
+  digitalWrite(in1, LOW);   
   digitalWrite(in2, HIGH);
-  delay(200); 
-  Serial.println("Stop");    
   Serial.println(counter);
+  }
+  Serial.println("Reverse out of the loop");  
+  digitalWrite(in1, HIGH);   
+  digitalWrite(in2, HIGH);  
   delay(2000);              // wait for a second
-
+  Serial.println(counter);
   
 }
+
+
+
